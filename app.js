7ffc0d9e47f6b1dc53368173433766d5405f7f5a
sushaz9 <susannahelisabeth1@gmail.com>
2023-10-03 15:54:45 +0100

    changejs

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
+let userClicks = 0;
+let maxClicks = 25;
+
 let productContainer = document.querySelector("section");
 let image1 = document.querySelector("section img:first-child");
 let image2 = document.querySelector("section img:nth-child(2)");
 let image3 = document.querySelector("section img:nth-child(3)");
 
+let renderResults = document.querySelector(".clicks-allowed");
+
 function Product(name, src) {
   this.name = name;
-  this.src = src;
+  this.src = `./images/${name}.jpg`;
   this.views = 0;
   this.clicks = 0;
 }
+
+const allProducts = [
+  new Product("bag"),
+  new Product("banana"),
+  new Product("bathroom"),
+  new Product("boots"),
+  new Product("breakfast"),
+  new Product("bubblegum"),
+  new Product("chair"),
+  new Product("cthulhu"),
+  new Product("dog-duck"),
+  new Product("dragon"),
+  new Product("pen"),
+  new Product("pet-sweep"),
+  new Product("scissors"),
+  new Product("shark"),
+  new Product("sweep"),
+  new Product("tauntaun"),
+  new Product("unicorn"),
+  new Product("water-can"),
+  new Product("wine-glass"),
+];
 
 function getRandomIndex() {
   return Math.floor(Math.random() * allProducts.length);
@@ -18,23 +45,27 @@ function renderProducts() {
   let product1Index = getRandomIndex();
   let product2Index = getRandomIndex();
   let product3Index = getRandomIndex();
+
+  while (
+    product1Index === product2Index ||
+    product1Index === product3Index ||
+    product2Index === product3Index
+  ) {
+    product2Index = getRandomIndex();
+    product3Index - getRandomIndex();
+  }
+
+  image1.src = allProducts[product1Index].src;
+  image2.src = allProducts[product2Index].src;
+  image3.src = allProducts[product3Index].src;
+  image1.alt = allProducts[product1Index].name;
+  image2.alt = allProducts[product2Index].name;
+  image3.alt = allProducts[product3Index].name;
+
+  allProducts[product1Index].views++;
+  allProducts[product2Index].views++;
+  allProducts[product3Index].views++;
 }
-
-while (product1Index === product2Index) {
-  product2Index = getRandomIndex();
-}
-
-image1.src = allProducts[product1Index].src;
-image2.src = allProducts[product2Index].src;
-image3.src = allProducts[product3Index].src;
-
-image1.alt = allProducts[product1Index].name;
-image2.alt = allProducts[product2Index].name;
-image3.alt = allProducts[product3Index].name;
-
-allProducts[product1Index].views++;
-allProducts[product2Index].views++;
-allProducts[product3Index].views++;
 
 function handleProductClick(event) {
   let clickedProduct = event.target.alt;
@@ -43,6 +74,13 @@ function handleProductClick(event) {
     alert("Please click on an image");
   } else {
     renderProducts();
+  }
+
+  userClicks++;
+
+  if (userClicks >= maxClicks) {
+    alert("You have run out of votes");
+    return;
   }
 
   for (let i = 0; i < allProducts.length; i++) {
@@ -56,41 +94,44 @@ function handleProductClick(event) {
   }
 }
 
-const allProducts = [
-  new Product(
-    "Bag",
-    "https://github.com/Wollivan/lab11-assets/blob/main/bag.jpg?raw=true"
-  ),
-  new Product(
-    "Breakfast",
-    "https://github.com/Wollivan/lab11-assets/blob/main/breakfast.jpg?raw=true"
-  ),
-  new Product(
-    "Unicorn Meat",
-    "https://github.com/Wollivan/lab11-assets/blob/main/unicorn.jpg?raw=true"
-  ),
-  new Product(
-    "Tauntaun",
-    "https://github.com/Wollivan/lab11-assets/blob/main/tauntaun.jpg?raw=true"
-  ),
-  new Product(
-    "Banana",
-    "https://github.com/Wollivan/lab11-assets/blob/main/banana.jpg?raw=true"
-  ),
-  new Product(
-    "Shark",
-    "https://github.com/Wollivan/lab11-assets/blob/main/shark.jpg?raw=true"
-  ),
-  new Product(
-    "Bathroom",
-    "https://github.com/Wollivan/lab11-assets/blob/main/bathroom.jpg?raw=true"
-  ),
-  new Product(
-    "Bubblegum",
-    "https://github.com/Wollivan/lab11-assets/blob/main/bubblegum.jpg?raw=true"
-  ),
-];
-
 productContainer.addEventListener("click", handleProductClick);
 
+function showResults() {
+  for (let i = 0; i < allProducts.length; i++) {
+    const li = document.createElement("li");
+    const product = allProducts[i];
+    li.textContent = `${product.name} was viewed ${product.views} times, and clicked ${product.clicks} times`;
+    viewResults.appendChild(li);
+  }
+}
+
+const viewResults = document.getElementById("click-here");
+renderResults.addEventListener("click", showResults);
+
 renderProducts();
+
+// function renderChart() {
+/* <script>
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+</script>  */
