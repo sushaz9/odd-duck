53b78210f479e32cafc9a2bd9f8ee41c65a02254
sushaz9 <susannahelisabeth1@gmail.com>
2023-10-05 14:30:30 +0100

    fixchart

--- app.js
+++ app.js
@@ -91,7 +91,6 @@ function handleProductClick(event) {
 
   if (userClicks >= maxClicks) {
     alert("You have run out of votes");
-    renderChart();
     localStorage.setItem("allProducts", JSON.stringify(allProducts));
     return;
   }
@@ -124,39 +123,3 @@ function showResults() {
 
 const viewResults = document.getElementById("click-here");
 renderResults.addEventListener("click", showResults);
-
-function renderChart() {
-  const ctx = document.getElementById("myChart");
-
-  const labels = [];
-  const views = [];
-  const clicks = [];
-
-  for (let i = 0; i < allProducts.length; i++) {
-    labels.push(allProducts[i].name);
-    views.push(allProducts[i].views);
-    clicks.push(allProducts[i].clicks);
-  }
-
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: "# of Votes",
-          data: views,
-          borderWidth: 1,
-        },
-        {
-          type: "bar",
-          label: "# of clicks",
-          data: clicks,
-          borderWidth: 1,
-        },
-      ],
-    },
-  });
-}
-
-renderProducts();
--- two.js
+++ two.js
@@ -1,8 +1,46 @@
-localStorage.setItem("allProducts", JSON.stringify(state.allProducts));
-
-let allProducts = localStorage.getItem("allProducts");
-console.log(JSON.parse(productsFromLs));
+/*console.log(JSON.parse(productsFromLs));
 
 let productsFromLs = new Product(JSON.parse(productsFromLs).name);
 
 //productButton.addEventListener("submit", handleSubmit);
+
+function rendermyChart() {
+  const allProducts = localStorage.getItem("allProducts");
+}*/
+
+function renderChart() {
+  const allProducts = JSON.parse(localStorage.getItem("allProducts"));
+  const ctx = document.getElementById("myChart");
+
+  const labels = [];
+  const views = [];
+  const clicks = [];
+
+  for (let i = 0; i < allProducts.length; i++) {
+    labels.push(allProducts[i].name);
+    views.push(allProducts[i].views);
+    clicks.push(allProducts[i].clicks);
+  }
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: views,
+          borderWidth: 1,
+        },
+        {
+          type: "bar",
+          label: "# of clicks",
+          data: clicks,
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+}
+
+renderChart();
